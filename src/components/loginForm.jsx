853f112a2bac94167517788d94be60c384dc14e0
Kabeer-developer <kabeeratwork7@gmail.com>
2026-08-05 19:21:39 +0530

    removed email option

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -3,37 +3,30 @@ import { useState } from "react";
 function LoginForm(){
     const username = "kabeer";
     const password = "Kabeer@123";
-    const email = "kabeer@demo.com";
-
+    
     const [user,setUser] = useState("");
     const [pass,setPass] = useState("");
-    const [mail,setMail] = useState("");
-    const [msg,setMsg] = useState("Please Login");
+    const [msg,setMsg] = useState("Login to continue");
 
-  function handleSubmit(e){
-    e.preventDefault();
-    if(user == username && pass == password || mail == email && pass == password){
-      setMsg("Welcome to Dashboard");
-    } else {
-      setMsg("Please check credencials");
+    function formSubmit(e){
+      e.preventDefault();
+
+      if(user==username && pass == password){
+        setMsg("Welcome to Dashboard");
+      } else {
+        setMsg("Check credencials");
+      }
     }
-  }
+    return(
+      <form onSubmit={formSubmit}>
+        <input type="text" value={user} placeholder="Username" onChange={(e)=> setUser(e.target.value)}></input>
+        <input type="password" value={pass} placeholder="Password" onChange={(e)=> setPass(e.target.value)}></input>
+        <button>Login</button>
 
-  return(
-    <form onSubmit={handleSubmit}>
-      <input placeholder="Enter username/mail" 
-        onChange={(e)=> {
-        setMail(e.target.value);
-        setUser(e.target.value);
-      }}>
-      </input>
-      <input placeholder="Enter Password"
-      onChange={(e)=> setPass(e.target.value)}>
-      </input>
-      <button type="submit">Login</button>
-      <h1>{msg}</h1>
-    </form>
-  )
+        <p>{msg}</p>
+      </form>
+    )
+    
 }
 
 export default LoginForm;
