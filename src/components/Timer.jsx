11bdae8a1c1c84fbcee5c3102679b1121d9a4586
Kabeer-developer <kabeeratwork7@gmail.com>
2026-08-15 19:03:17 +0530

    Unwanted code removed

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,11 +1,11 @@
 import { useEffect, useState } from "react";
 
 export default function Timer() {
-    const [timer,setTimer]=useState(10);
+    const [timer,setTimer]=useState(-50);
     const [isRunning,setIsRunning]=useState(false);
 
     useEffect(()=>{
-        if(timer<0) return;
+        if(timer<0) return window.alert("Invalid time");
         let interval;
         if(isRunning){
             if(timer==0) window.alert("Timer Completed");
@@ -13,11 +13,7 @@ export default function Timer() {
                 interval =setInterval(() => {
                     setTimer((prev)=> prev-1);
                 }, 1000);
-            } else {
-                clearInterval(interval);
-                setIsRunning(false);
-                setTimer(0);
-            }
+            } 
         }
         return ()=> clearInterval(interval);
     })
@@ -26,7 +22,6 @@ export default function Timer() {
         <div>
           {timer}
           <button onClick={()=> setIsRunning(true)}>Start</button>
-
         </div>
     );
 }
